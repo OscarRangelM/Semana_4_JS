541975211e7f1f7bde37dff1770a1db428eb3db4
Rangel Morales Oscar Mauricio <92708951+OscarRangelM@users.noreply.github.com>
2022-03-29 19:58:19 -0600

    Shiny
    Agregamos el valor shiny

--- JS/index.js
+++ JS/index.js
@@ -20,6 +20,22 @@ const fetchPokemon = () => {
         }
     });
 }
+const fetchShiny = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        return res.json();
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_shiny;
+            pokeImage(pokeImg);
+            console.log(pokeImg);
+        }
+    });
+}
 const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImg");
     pokePhoto.src = url;
